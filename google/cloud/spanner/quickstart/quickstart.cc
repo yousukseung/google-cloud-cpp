ec7cf6f3a328ee0f602f9c1448964d745d74470c
Yousuk Seung <ysseung@google.com>
2024-08-12 15:31:28 -0700

    Wait before terminating

--- google/cloud/spanner/quickstart/quickstart.cc
+++ google/cloud/spanner/quickstart/quickstart.cc
@@ -15,28 +15,38 @@
 //! [all]
 #include "google/cloud/spanner/client.h"
 #include <iostream>
+#include "absl/time/clock.h"
 
 int main(int argc, char* argv[]) {
-  if (argc != 4) {
+  if (argc < 4) {
     std::cerr << "Usage: " << argv[0]
               << " project-id instance-id database-id\n";
     return 1;
   }
 
   namespace spanner = ::google::cloud::spanner;
-  spanner::Client client(
-      spanner::MakeConnection(spanner::Database(argv[1], argv[2], argv[3])));
+  {
+    spanner::Client client(
+        spanner::MakeConnection(spanner::Database(argv[1], argv[2], argv[3])));
 
-  auto rows =
-      client.ExecuteQuery(spanner::SqlStatement("SELECT 'Hello World'"));
+    auto rows =
+        client.ExecuteQuery(spanner::SqlStatement("SELECT 'Hello World'"));
 
-  for (auto const& row : spanner::StreamOf<std::tuple<std::string>>(rows)) {
-    if (!row) {
-      std::cerr << row.status() << "\n";
-      return 1;
+    for (auto const& row : spanner::StreamOf<std::tuple<std::string>>(rows)) {
+      if (!row) {
+        std::cerr << row.status() << "\n";
+        return 1;
+      }
+      std::cout << std::get<0>(*row) << "\n";
     }
-    std::cout << std::get<0>(*row) << "\n";
+    std::cout << "YSSEUNG Destroying client." << "\n" << std::flush;
   }
+  if (argc >= 5) {
+    int sec_to_sleep = atoi(argv[4]);
+    std::cout << "YSSEUNG Waiting for " << sec_to_sleep << "s\n" << std::flush;
+    absl::SleepFor(absl::Seconds(sec_to_sleep));
+  }
+  std::cout << "YSSEUNG Terminating process." << "\n" << std::flush;
 
   return 0;
 }
